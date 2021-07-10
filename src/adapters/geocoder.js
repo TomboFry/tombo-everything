86321436d405b5bbeb3ba01870e925a854028b27
Tom Gardiner <tom@tombofry.co.uk>
2021-07-10 22:56:00 +0100

    Add geocoder

---
+++ src/adapters/geocoder.js
@@ -0,0 +1,16 @@
+import NodeGeocoder from 'node-geocoder';
+
+let geocoder = null;
+
+/**
+ * @returns {NodeGeocoder.Geocoder}
+ */
+export function getGeocoder () {
+	if (geocoder !== null) return geocoder;
+
+	geocoder = NodeGeocoder({
+		provider: 'openstreetmap',
+	});
+
+	return geocoder;
+}
