48d6872988113bc2c57f069e70ef09242622fa0b
Tom Gardiner <tom@tombofry.co.uk>
2021-07-12 18:59:30 +0100

    Let frontend (Preact) handle all further requests

--- src/routers/frontend.js
+++ src/routers/frontend.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const router = express.Router();
 
-router.use(
-	express.static('public'),
-	(_req, res) => res.status(404).send('<h1>404 Not Found</h1>'),
-);
+router.use('/', express.static('public'));
+
+router.use('*', (_req, res) => {
+	const file = path.resolve('./public/index.html');
+	res.sendFile(file);
+});
 
 export default router;
