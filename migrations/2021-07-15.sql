5ab27c7cade95373537d30f09a60a1be9c713014
Tom Gardiner <tom@tombofry.co.uk>
2021-07-15 20:28:20 +0100

    Add updated_at column to games table

--- migrations/2021-07-15.sql
+++ migrations/2021-07-15.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS "games" (
 	"name"	TEXT NOT NULL,
 	"playtime_mins"	INTEGER NOT NULL,
 	"created_at"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"updated_at"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"device_id"	TEXT NOT NULL,
 	PRIMARY KEY("id"),
 	FOREIGN KEY("device_id") REFERENCES "devices"("id")
