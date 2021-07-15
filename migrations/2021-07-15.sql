dcb403521a253d9355a16bdb019af5bb34df1c96
Tom Gardiner <tom@tombofry.co.uk>
2021-07-15 17:07:40 +0100

    Add created_at column to games table

--- migrations/2021-07-15.sql
+++ migrations/2021-07-15.sql
@@ -1,10 +1,12 @@
 BEGIN TRANSACTION;
-CREATE TABLE "games" (
+CREATE TABLE IF NOT EXISTS "games" (
 	"id"	TEXT NOT NULL UNIQUE,
 	"name"	TEXT NOT NULL,
 	"playtime_mins"	INTEGER NOT NULL,
 	"currently_playing"	INTEGER NOT NULL DEFAULT 0,
+	"created_at"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"device_id"	TEXT NOT NULL,
+	PRIMARY KEY("id"),
 	FOREIGN KEY("device_id") REFERENCES "devices"("id")
 );
 COMMIT;
