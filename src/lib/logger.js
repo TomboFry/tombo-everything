ddb1c1bccf3c7a8542ef3e686766c9f0d6342bd4
Tom Gardiner <tom@tombofry.co.uk>
2021-07-21 09:05:15 +0100

    Abstract formatDate into separate file

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,30 +1,5 @@
 import util from 'util';
-
-/**
- * @param {string} str
- * @param {number} length
- * @param {string} pad
- */
-function padString (str, length, pad = '0') {
-	let newStr = str.toString();
-	while (newStr.length < length) {
-		newStr = `${pad}${newStr}`;
-	}
-	return newStr;
-}
-
-/** @param {Date} date */
-function formatDate (date) {
-	const year = padString(date.getFullYear(), 4);
-	const mon = padString(date.getMonth() + 1, 2);
-	const day = padString(date.getDate(), 2);
-
-	const hour = padString(date.getHours(), 2);
-	const mins = padString(date.getMinutes(), 2);
-	const secs = padString(date.getSeconds(), 2);
-
-	return `${year}-${mon}-${day} ${hour}:${mins}:${secs}`;
-}
+import formatDate from './formatDate.js';
 
 const levels = {
 	debug: 0,
