a0cbc96eb9a0c64044e61b23e0fdfdc79846ab97
Tom Gardiner <tom@tombofry.co.uk>
2021-07-23 19:35:33 +0100

    Use description if merchant name is unavailable

--- src/routers/purchases.js
+++ src/routers/purchases.js
@@ -14,7 +14,7 @@ router.post('/', (req, res) => {
 			throw new Error('Unrecognised webhook type');
 		}
 
-		const { account_id, amount, created, currency, category, merchant } = data;
+		const { account_id, amount, created, currency, category, merchant, description } = data;
 
 		if (account_id !== process.env.TOMBOIS_MONZO_ACCOUNT_ID) {
 			throw new Error(`Unexpected account ID '${account_id}'`);
@@ -23,9 +23,10 @@ router.post('/', (req, res) => {
 		const amountPounds = -(Number(amount) || 0) / 100;
 		const createdAt = new Date(created).toISOString();
 		const deviceId = process.env.TOMBOIS_DEFAULT_DEVICE_ID;
+		const merchantName = merchant?.name ?? description;
 
-		log.info(`Adding purchase of ${amountPounds} ${currency} at ${merchant.name}`);
-		insertPurchase(amountPounds, merchant.name, category, currency, createdAt, deviceId);
+		log.info(`Adding purchase of ${amountPounds} ${currency} at ${merchantName}`);
+		insertPurchase(amountPounds, merchantName, category, currency, createdAt, deviceId);
 
 		res.send({ status: 'ok' });
 	} catch (err) {
