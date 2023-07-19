8c44a7f48f6b6d9d302fdc805746a732a32f72ef
Tom Gardiner <tom@tombofry.co.uk>
2023-07-19 14:14:08 +0100

    List "all" sleep on internal CRUD
    Because the default was still set to 10 days, you couldn't see
    historical data on the internal view. 10000 days is approximately 30
    years, so this should be good enough!

--- src/routers/internal/sleep.js
+++ src/routers/internal/sleep.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 	const { page = 0 } = req.query;
 	const pagination = handlebarsPagination(page, countSleepCycles());
 
-	const sleep = getSleepCycles({ page });
+	const sleep = getSleepCycles({ page, days: 10000 });
 
 	res.render('internal/sleep', { sleep, pagination });
 });
