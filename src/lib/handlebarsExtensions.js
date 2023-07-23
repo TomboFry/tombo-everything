4977946fb852e3ec14ab772c0f03450cd329c317
Tom Gardiner <tom@tombofry.co.uk>
2023-07-23 13:31:48 +0100

    Use `person` instead of `name`
    It clashes with the internal CRUD input names, which means you can't
    create any new records!

--- src/lib/handlebarsExtensions.js
+++ src/lib/handlebarsExtensions.js
@@ -31,12 +31,12 @@ export function externalRoot () {
 	return process.env.TOMBOIS_SERVER_EXTERNAL_URI;
 }
 
-export function name () {
+export function person () {
 	return process.env.TOMBOIS_PERSON_NAME;
 }
 
 export const helpers = {
 	xif,
 	externalRoot,
-	name,
+	person,
 };
