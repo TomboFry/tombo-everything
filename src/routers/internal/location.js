422c787f3b47030f16c267ea02a48cce962f4182
TomboFry <tom@tombofry.co.uk>
2024-04-02 18:04:20 +0100

    Use a heatmap to display location history
    TODO: Add a toggle between lines and heatmap, and add date picker

--- src/routers/internal/location.js
+++ src/routers/internal/location.js
@@ -16,56 +16,65 @@ router.get('/', (req, res) => {
 		throw new Error('Date Start and End must be an ISO date formatted string (eg. "2024-01-02"');
 	}
 
-	const paths = [ [] ];
-	const points = [];
-
 	const location = getLocationHistory(date_start, date_end);
 
 	const avgLat = location.reduce((acc, cur) => acc + cur.lat, 0) / location.length;
 	const avgLong = location.reduce((acc, cur) => acc + cur.long, 0) / location.length;
 
-	location.forEach(location => {
-		const pathIndex = paths.length - 1;
+	// const paths = [ [] ];
+	const points = [];
+	// location.forEach(location => {
+	// 	const pathIndex = paths.length - 1;
 
-		// Always add the path if it's currently empty
-		if (paths[pathIndex].length === 0) {
-			if (location.city !== null) {
-				points.push({
-					latlng: [ location.lat, location.long ],
-					title: location.city,
-				});
-			}
-			paths[pathIndex].push([ location.lat, location.long ]);
-			return;
-		}
+	// 	// Always add the path if it's currently empty
+	// 	if (paths[pathIndex].length === 0) {
+	// 		if (location.city !== null) {
+	// 			points.push({
+	// 				latlng: [ location.lat, location.long ],
+	// 				title: location.city,
+	// 			});
+	// 		}
+	// 		paths[pathIndex].push([ location.lat, location.long ]);
+	// 		return;
+	// 	}
 
-		const currentPath = paths[pathIndex];
-		const idx = currentPath.length - 1;
-		const diffLat = Math.abs(location.lat - currentPath[idx][0]);
-		const diffLong = Math.abs(location.long - currentPath[idx][1]);
+	// 	const currentPath = paths[pathIndex];
+	// 	const idx = currentPath.length - 1;
+	// 	const diffLat = Math.abs(location.lat - currentPath[idx][0]);
+	// 	const diffLong = Math.abs(location.long - currentPath[idx][1]);
 
-		// Start a new path for big jumps
-		if (diffLat > 0.2 || diffLong > 0.2) {
-			if (location.city !== null) {
-				points.push({
-					latlng: [ location.lat, location.long ],
-					title: location.city,
-				});
-			}
-			if (paths[pathIndex].length === 1) {
-				paths.pop();
-			}
-			paths.push([ [ location.lat, location.long ] ]);
-			return;
-		}
+	// 	// Start a new path for big jumps
+	// 	if (diffLat > 0.2 || diffLong > 0.2) {
+	// 		if (location.city !== null) {
+	// 			points.push({
+	// 				latlng: [ location.lat, location.long ],
+	// 				title: location.city,
+	// 			});
+	// 		}
+	// 		if (paths[pathIndex].length === 1) {
+	// 			paths.pop();
+	// 		}
+	// 		paths.push([ [ location.lat, location.long ] ]);
+	// 		return;
+	// 	}
 
-		// Skip very minor movements, to reduce noise and response size.
-		if (diffLat < 0.0001 || diffLong < 0.0001) {
-			return;
-		}
+	// 	// Skip very minor movements, to reduce noise and response size.
+	// 	if (diffLat < 0.0001 || diffLong < 0.0001) {
+	// 		return;
+	// 	}
 
-		paths[pathIndex].push([ location.lat, location.long ]);
-	});
+	// 	paths[pathIndex].push([ location.lat, location.long ]);
+	// });
+
+	// This is some weird calculation I determined in Excel that smoothly
+	// goes from 1.0 to 0.3 between 0 and 100000 points, in a curve, based
+	// on the following formula (assuming it's clamped at 100000)
+	// =(POWER(((100000 - x) / 10000), 2) / 142.857142857) + 0.3
+	const intensity = location.length <= 100000
+		? (Math.pow((100000 - location.length) / 10000, 2) / 142.857142857) + 0.3
+		: 0.3;
+
+	const paths = location.map(loc => [ loc.lat, loc.long, intensity ]);
 
 	res.render('internal/location', {
 		avgLat,
