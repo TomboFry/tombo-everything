eb61f343dd70f68e3bde987d9dd30e374fde1455
TomboFry <tom@tombofry.co.uk>
2024-06-03 00:28:39 +0100

    Pass in date to formatDate function
    Okay, time for bed, I typo'd the last commit message as well.

--- src/database/steps.js
+++ src/database/steps.js
@@ -94,6 +94,6 @@ export function updateSteps(id, step_count_total, created_at) {
 	return statement.run({
 		id,
 		step_count_total,
-		created_at: formatDate(created_at || Date.now()),
+		created_at: formatDate(new Date(created_at || Date.now())),
 	});
 }
