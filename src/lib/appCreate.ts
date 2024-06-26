be450f68709794e67af0d6199885fec228d9ee25
TomboFry <tom@tombofry.co.uk>
2024-06-26 17:18:51 +0100

    Upgrade @tombofry/stdlib (+ other dependencies)

--- src/lib/appCreate.ts
+++ src/lib/appCreate.ts
@@ -1,33 +1,13 @@
 import { resolve } from 'node:path';
 import { express as expressHelpers } from '@tombofry/stdlib';
-import express, { type Request, type RequestHandler } from 'express';
+import express from 'express';
 import { engine } from 'express-handlebars';
-import formBodyParser from './formParser.js';
 import { helpers } from './handlebarsExtensions.js';
-
-interface RequestRawBody extends Request {
-	rawBody: string;
-}
 
 const appCreate = () => {
 	const app = express();
 	app.use(expressHelpers.rawBody);
-	app.use(((req: RequestRawBody, _res, next) => {
-		try {
-			req.body = JSON.parse(req.rawBody);
-		} catch (err) {
-			/* Do nothing */
-		}
-		try {
-			if (!req.body) {
-				req.body = formBodyParser(req.rawBody);
-			}
-		} catch (err) {
-			/* do nothing */ console.error(err);
-		}
-
-		next();
-	}) as RequestHandler);
+	app.use(expressHelpers.parseRawBody);
 	app.use(expressHelpers.logEntry(console.info));
 
 	// Set up frontend
